54ee32feee7ecc76e42fbe69aea13cef1bf1710c
ZynabBinteAzhar <ntodo222@gmail.com>
2024-10-23 11:10:52 +0500

    simple&&dynamic routing with 404Page

--- testProject/src/app/app-routing.module.ts
+++ testProject/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { ServiceComponent } from './service/service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"services", component:ServicesComponent},
+  {path:"services/:id", component:ServiceComponent},
+  {path:"**", component:NoPageFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- testProject/src/app/app.module.ts
+++ testProject/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    NoPageFoundComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,

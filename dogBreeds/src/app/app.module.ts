7f848c5cbfe5c8a31193c21cef85d8e91431ab20
ZynabBinteAzhar <ntodo222@gmail.com>
2024-12-04 16:15:15 +0500

    loader using interceptor

--- dogBreeds/src/app/app.module.ts
+++ dogBreeds/src/app/app.module.ts
@@ -5,20 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BreedComponent } from './components/breed/breed.component';
 import { HistoryComponent } from './components/history/history.component';
-import {HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { LoaderComponent } from './components/loader/loader.component';
+import { LoaderInterceptor } from './interceptors/loader.interceptor';
+import { LoaderService } from './shared/loader.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     BreedComponent,
     HistoryComponent,
+    LoaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    LoaderService,
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
